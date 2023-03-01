0faa1ee6936058d5518b9e7c50eb9cb8be52d447
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-01 15:36:36 +0100

    first commit

---
+++ src/main/java/com/niji/lille/portailLille/PortailLilleApplication.java
@@ -0,0 +1,13 @@
+package com.niji.lille.portailLille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailLilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailLilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niji/lille/portailLille/PortailLilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niji.lille.portailLille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailLilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
