b516d6e5251b73f059d08471e0f8220643e9c45a
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-02 11:38:08 +0100

    feat: creation serviceImp avec mise en place de la logique pour l'entity Event

--- src/main/java/com/niji/lille/portailLille/repository/EventRepository.java
+++ src/main/java/com/niji/lille/portailLille/repository/EventRepository.java
@@ -1,8 +1,13 @@
 package com.niji.lille.portailLille.repository;
 
+import com.niji.lille.portailLille.entity.Category;
 import com.niji.lille.portailLille.entity.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EventRepository extends MongoRepository<Event, String> {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findByDateDebut(LocalDate dateDebut);
+    List<Event> findByCategory(Category category);
 }
---
+++ src/main/java/com/niji/lille/portailLille/servcie/serviceImp/EventServiceImp.java
@@ -0,0 +1,91 @@
+package com.niji.lille.portailLille.servcie.serviceImp;
+
+import com.niji.lille.portailLille.entity.Category;
+import com.niji.lille.portailLille.entity.Event;
+import com.niji.lille.portailLille.repository.EventRepository;
+import com.niji.lille.portailLille.servcie.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class EventServiceImp implements EventService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventService.class);
+    private final EventRepository eventRepository;
+
+    public EventServiceImp(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public List<Event> findALl() {
+        logger.info("Affiche l'ensemble des events existants");
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event save(Event entity) {
+        logger.info("Sauvegarde, enregistrement d'un nouvel event en fonction des renseignements fournis par " +
+                "l'utilisateur dans le body " + entity);
+        return eventRepository.save(entity);
+    }
+
+    @Override
+    public List<Event> findByDateDebut(LocalDate dateDebut) {
+        logger.info("Recherche d'un event par dateDebut renseignée: " + dateDebut);
+        return eventRepository.findByDateDebut(dateDebut);
+    }
+
+    @Override
+    public List<Event> findByCategory(Category category) {
+        logger.info("Recherche d'un event par la categorie renseignée: " + category);
+        return eventRepository.findByCategory(category);
+    }
+
+    @Override
+    public Event findById(String id) {
+        logger.info("Recherche d'un event par son id: " + id);
+        return eventRepository.findById(id).orElseThrow(() -> {
+            logger.warn("l'id: " + id + " est invalide");
+            return new ResponseStatusException((HttpStatus.NOT_FOUND));
+        });
+    }
+
+    @Override
+    public Event modifierById(String id, Event entity) {
+        logger.info("Modification d'un event avec les elements suivants :" + entity);
+        Event event = this.findById(id);
+        if (entity.getCategory() == null) {
+            event.setCategory(event.getCategory());
+        } else event.setCategory(entity.getCategory());
+
+        if (entity.getDateDebut() == null) {
+            event.setDateDebut(event.getDateDebut());
+        } else event.setDateDebut((entity.getDateDebut()));
+
+        if (entity.getHeure() == null) {
+            event.setHeure(event.getHeure());
+        } else event.setHeure(entity.getHeure());
+
+        if (entity.getLieu() == null) {
+            event.setLieu(event.getLieu());
+        } else event.setLieu(entity.getLieu());
+
+        if (entity.getCommentaire() == null) {
+            event.setCommentaire(event.getCommentaire());
+        } else event.setCommentaire(entity.getCommentaire());
+
+        return this.save(event);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        logger.info("l'event portant l'id:" + id + " a été supprimé");
+        eventRepository.deleteById(id);
+    }
+
+}
