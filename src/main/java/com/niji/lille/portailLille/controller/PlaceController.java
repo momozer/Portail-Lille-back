58d0c23f8f6fe20049c6f26c824ab6101160d05f
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-03 14:45:18 +0100

    feat : développement du controller pour la classe Place avec la norme REST

--- src/main/java/com/niji/lille/portailLille/controller/PlaceController.java
+++ src/main/java/com/niji/lille/portailLille/controller/PlaceController.java
@@ -1,4 +1,45 @@
 package com.niji.lille.portailLille.controller;
 
+import com.niji.lille.portailLille.entity.Place;
+import com.niji.lille.portailLille.servcie.PlaceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/place")
 public class PlaceController {
+    private final PlaceService placeService;
+
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("")
+    public List<Place> getAllPlaces() {
+        return placeService.getAllPlaces();
+    }
+
+    @GetMapping("{id}")
+    public Place getPlaceById(String id) {
+        return placeService.getPlaceById(id);
+    }
+
+    @PostMapping("")
+    public Place createPlace(Place entity) {
+        return placeService.createPlace(entity);
+    }
+
+    @PatchMapping("{id}")
+    public Place updatePlace(String id, Place entity) {
+        return placeService.updatePlace(id, entity);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePlace(String id) {
+        placeService.deletePlace(id);
+    }
+
 }
