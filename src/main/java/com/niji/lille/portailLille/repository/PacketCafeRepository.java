ae393d290f234a0dac944c8c205b16f5ed09780f
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-06 10:42:15 +0100

    feat : creation et devleoppement du Service impl et service pour la gestion de la class PacketCaffe

--- src/main/java/com/niji/lille/portailLille/repository/PacketCafeRepository.java
+++ src/main/java/com/niji/lille/portailLille/repository/PacketCafeRepository.java
@@ -3,5 +3,5 @@ package com.niji.lille.portailLille.repository;
 import com.niji.lille.portailLille.entity.PacketCafe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CafeRepository extends MongoRepository<PacketCafe, String> {
+public interface PacketCafeRepository extends MongoRepository<PacketCafe, String> {
 }
--- src/main/java/com/niji/lille/portailLille/servcie/serviceImp/PacketCafeServiceImp.java
+++ src/main/java/com/niji/lille/portailLille/servcie/serviceImp/PacketCafeServiceImp.java
@@ -1,9 +1,34 @@
 package com.niji.lille.portailLille.servcie.serviceImp;
 
+import com.niji.lille.portailLille.entity.PacketCafe;
+import com.niji.lille.portailLille.repository.PacketCafeRepository;
+import com.niji.lille.portailLille.servcie.PacketCafeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PacketCafeServiceImp {
+public class PacketCafeServiceImp implements PacketCafeService {
 
+    private PacketCafeRepository packetCafeRepository;
 
+    @Override
+    public List<PacketCafe> findAll() {
+        return packetCafeRepository.findAll();
+    }
+
+    @Override
+    public PacketCafe findById(String id) {
+        return packetCafeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PacketCafe save(PacketCafe entity) {
+        return packetCafeRepository.save(entity);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        packetCafeRepository.deleteById(id);
+    }
 }
