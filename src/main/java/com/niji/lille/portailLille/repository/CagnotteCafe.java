afaac3c5b688bf0186118f73c203b44d4fa88f9f
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-06 14:52:20 +0100

    fix: suppression des class, services et repo pour cafe et refonte

--- src/main/java/com/niji/lille/portailLille/repository/CagnotteCafe.java
+++
@@ -1,7 +0,0 @@
-package com.niji.lille.portailLille.repository;
-
-import com.niji.lille.portailLille.entity.CafeCagnotte;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CagnotteCafe extends MongoRepository<CafeCagnotte, String> {
-}
--- src/main/java/com/niji/lille/portailLille/repository/PacketCafeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.niji.lille.portailLille.repository;
-
-import com.niji.lille.portailLille.entity.PacketCafe;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PacketCafeRepository extends MongoRepository<PacketCafe, String> {
-}
