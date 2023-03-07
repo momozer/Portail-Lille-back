3ceeea7e912e0cbbcd65514d1b40153e25f9d0ac
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-07 16:18:28 +0100

    fix : suppression de certains decorateur et certaines methodes

--- src/main/java/com/niji/lille/portailLille/repository/IdeeRepository.java
+++ src/main/java/com/niji/lille/portailLille/repository/IdeeRepository.java
@@ -4,7 +4,6 @@ import com.niji.lille.portailLille.entity.Idee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface IdeeRepository extends MongoRepository<Idee, String> {
 
