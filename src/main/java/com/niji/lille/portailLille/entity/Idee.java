65c63e49568ad0e16e012b5ab639471dc51dca01
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-07 10:30:32 +0100

    feat : correction des entity + suppression entity cafe + ajout methode dans EVEnt

--- src/main/java/com/niji/lille/portailLille/entity/Idee.java
+++ src/main/java/com/niji/lille/portailLille/entity/Idee.java
@@ -15,4 +15,52 @@ public class Idee {
     private String id;
     private Integer vote;
     private String titre;
+    private String description;
+
+
+//constructors:
+    public Idee() {
+    }
+
+    public Idee(String id, Integer vote, String titre, String description) {
+        this.id = id;
+        this.vote = vote;
+        this.titre = titre;
+        this.description = description;
+    }
+
+    //getter et setter:
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getVote() {
+        return vote;
+    }
+
+    public void setVote(Integer vote) {
+        this.vote = vote;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
